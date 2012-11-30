e85bdab5a04f2d895a53ebfbb9b266c8560bea08
unknown <DECANS@.(none)>
2012-11-30 11:12:45 +0530

    project item

--- src/com/millenniumit/mx/data/relman/service/RelmanCurrentVersionService.java
+++
@@ -1,9 +0,0 @@
-package com.millenniumit.mx.data.relman.service;
-
-import java.util.List;
-
-import com.millenniumit.mx.data.relman.domain.RelmanCurrentVersion;
-
-public interface RelmanCurrentVersionService {
-	public List<RelmanCurrentVersion> getRelmanCurrentVersions();
-}
